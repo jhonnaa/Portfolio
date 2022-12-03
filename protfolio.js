4ca2fb4b7435d5ebb7755e80c67b1d3f6d93df0b
Jhonnaa <jonnyemeni@gmail.com>
2022-12-02 21:21:03 -0300

    primer commit

---
+++ protfolio.js
@@ -0,0 +1,32 @@
+document.getElementById("1").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="Tengo años tranajando con esta herramienta, una de las primeras que aprendi con buenas practicas y semantica"
+})
+
+document.getElementById("2").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="LLevo tiempo usando esta herramienta, realizo mis proyectos diseñando preferentemente en vanilla "
+})
+
+document.getElementById("3").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="una de mis preferidas"
+})
+
+document.getElementById("4").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="chau"
+})
+
+document.getElementById("5").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="hola"
+})
+
+document.getElementById("6").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="chau"
+})
+
+document.getElementById("7").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="hola"
+})
+
+document.getElementById("8").addEventListener("mouseover" , ()=>{
+    document.querySelector(".short-description p").innerHTML ="chau"
+})
+
